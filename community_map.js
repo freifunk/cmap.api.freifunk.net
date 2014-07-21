e62be4e8c413b73fb2e8a7cbe9733341cd9f02e0
andibraeu <ab@andi95.de>
2014-07-21 20:38:35 +0200

    fixed map attributions to the needs of mapbox, fixes #5

--- community_map.js
+++ community_map.js
@@ -128,7 +128,7 @@ var FFCommunityMapWidget = function(settings, map_options, link) {
   );
   
   var mapboxLayer = L.tileLayer('https://{s}.tiles.mapbox.com/v3/freienetzwerke.i2lgkb76/{z}/{x}/{y}.png', {
-    attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
+attribution: '<a href="https://www.mapbox.com/about/maps/" target="_blank">&copy; Mapbox &copy; OpenStreetMap</a> <a class="mapbox-improve-map" href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a>'
   });
   
   var osmlayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
